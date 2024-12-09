c515a88f576581cd3f5c0017d38a5ad5e4170f2f
Piotr <148041406+molk915@users.noreply.github.com>
2024-12-09 18:35:44 +0100

    drugi commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Button, StyleSheet, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Płock GO</Text>
+      <Button 
+        title="Sprawdź Połączenia" 
+        onPress={() => Alert.alert("Połączenia", "Tutaj będą połączenia!")} 
+      />
+      <Button 
+        title="Kup Bilet" 
+        onPress={() => Alert.alert("Kup Bilet", "Tutaj będzie opcja kupna biletu!")} 
+        color="#0047AB"
+      />
     </View>
   );
 }
@@ -13,8 +21,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#cce6ff',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#0047AB',
   },
 });
